a45ce3be8fe4560f7690420894cd4a29404516f9
enaaomanovic <81486261+enaaomanovic@users.noreply.github.com>
2023-09-29 15:26:53 +0200

    .

--- FitnessCentar/UI/fitness_admin/android/app/src/main/kotlin/com/example/fitness_admin/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fitness_admin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- FitnessCentar/UI/fitness_admin/lib/models/search_result.dart
+++
@@ -1,4 +0,0 @@
-class SearchResult<T> {
-  int count = 0;
-  List<T> result = [];
-}
